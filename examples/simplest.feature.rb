7123dcbb920c7ab80157029c1ae132a54674a36c
Jean-Michel Garnier <jean-michel@21croissants.com>
2013-02-23 18:42:20 +0100

    example of how it *will* look like

---
+++ examples/simplest.feature.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+Feature 'The Simplest Feature' do
+
+  Description <<TXT
+      A description
+TXT
+
+  Scenario "The Simplest Scenario" do
+
+    Given "some pre-conditions" do
+
+    end
+
+    When "something happens" do
+
+    end
+
+    Then "things should work as expected" do
+
+    end
+  end
+end
