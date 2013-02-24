0065500457970e204240df0431e79d05a60d9d84
Jean-Michel Garnier <jean-michel@21croissants.com>
2013-02-24 12:25:07 +0100

    first attempt of test first for GherkinFormatter

--- spec/unit/gherkin_formatter_spec.rb
+++ spec/unit/gherkin_formatter_spec.rb
@@ -1,5 +1,6 @@
 require_relative "spec_helper"
 
+require 'gherkin/parser/parser'
 require 'maniok_bdd/gherkin_formatter'
 
 module ManiokBdd
@@ -8,10 +9,16 @@ module ManiokBdd
 
     describe ".build" do
 
-      it "passes itself and the feature file to the Gherkin::Parser::Parser"
+      before do
+        feature_path = fixture_path "simplest.feature"
+        @gherkin_formatter = GherkinFormatter.build feature_path
+      end
 
       describe "builds a feature" do
-        it "with a name"
+        it "with a name" do
+          expect { @gherkin_formatter.feature.name == "The Simplest Feature" }
+        end
+
         it "with a description"
         xit "with a background"
         it "with a list of scenarios"
