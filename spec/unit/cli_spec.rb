506d91a9a5ed7b9f4fcbdee6b3f3ebb3d7cbb066
Jean-Michel Garnier <jean-michel@21croissants.com>
2013-02-24 10:23:38 +0100

    no need to use a generator, a Gherkin formatter will do the job

--- spec/unit/cli_spec.rb
+++ spec/unit/cli_spec.rb
@@ -8,19 +8,12 @@ module ManiokBdd
 
     context "When a valid existing .feature file is passed" do
 
-      before do
-        @generator = gimme "generator"
-        cli = Cli.new @generator
-        @returned_code = cli.run "fake.feature"
-      end
+      it "uses a GherkinFormatter to parse the feature file"
 
-      it "runs the generator" do
-        verify(@generator).generate(anything)
-      end
+      it "writes the result to a spec/acceptance/<feature-name>.feature.rb file"
 
-      it "returns 0 when generation was successfull" do
-        expect { @returned_code == 0 }
-      end
+      it "returns 0 to tell that it ran successfully"
+
     end
 
   end
